2c23e28e55bd86f199995117931358ec6ac18284
Bheeresh <142218770+Bheeresh-2001@users.noreply.github.com>
2023-11-01 14:43:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function sub(){
-    // let error
     let Name= document.getElementById("uName").value
     let pw = document.getElementById("pass").value
     let cp = document.getElementById("con").value
     if(Name == "" || Name == null){
-        // alert("Enter user Name")
-        // error = ("Enter Name")
         if(true){
         document.getElementById("na").innerHTML = `Enter a Name`;
         na.style.color='red'
@@ -16,7 +13,6 @@ function sub(){
         }
     }
     else if(pw == "" || pw == null){
-        // alert("Enter password")
         document.getElementById("pa").innerHTML = `Enter password`
         pa.style.color='red'
         pa.style.margin=`0 0 0 100px`;
@@ -24,7 +20,6 @@ function sub(){
 
     }
     else if(pw.length <=6){
-        // error = ("password must be have 6 character")
         document.getElementById("pa").innerHTML = `password must be have 6 character`;
         pa.style.color='red'
         return false
@@ -32,24 +27,19 @@ function sub(){
 
 
     else if(cp == "" || cp == null){
-        // alert("Enter password")
         document.getElementById("ca").innerHTML = `Enter Confirm password`;
         ca.style.color='red'
         return false
     }
     else if(cp!==pw){
-        // alert("Password not matched")
         document.getElementById("ca").innerHTML = `password not matched`;
         ca.style.color='red'
         return false
     }
     else{
-        // alert("Successfull")
         document.getElementById("su").innerHTML = `Subit Successfull`;
         su.style.color='green'
-
         return true
-
     }   
 }
 function show(){
@@ -59,17 +49,6 @@ function show(){
     }
     else{
         pass.type=("password")
-    }
-}
-
-
-function cnshow(){
-    let hide=document.getElementById("con")
-    if(hide.type=="password"){
-        con.type="text"
-    }
-    else{
-        con.type=("password")
     }
 }
 
